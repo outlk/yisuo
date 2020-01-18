bcf08565d9805a82f2f466c2966335c2928e08dc
OuTlK <outlk@qq.com>
2020-01-18 22:50:57 +0800

    增加Python脚本处理生成TeX再生成PDF文件。

---
+++ python/convert.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "gzshen"
+
+import jiphy
+import re
+
+
+def proc_js(lstAll):
+    begins = []
+    all_yuyan = []
+    for i, v in list(enumerate(lstAll)):
+        if re.findall("{", v):
+            # print(i,v)
+            begins.append(i+1)
+
+    print(begins)
+    for i in begins:
+        if i == 2626:
+            print(i)
+            pass
+        if (i+4) < len(lstAll) and re.findall("}", lstAll[i+4]):
+            x = {}
+            for j in range(4):
+                # tmp = lstAll[i+j].split(":")
+                a, b = lstAll[i+j].split(":")
+                a = a.encode('utf-8').decode('unicode_escape')
+                b = b.encode('utf-8').decode('unicode_escape')
+                # a, b = (a.lstrip(), b.lstrip().rstrip(","))
+                b=b.strip().rstrip(",")
+                a = re.findall(r"^\s*\"(.*)\"$", a)[0]
+                if a != "index":
+                    b = re.findall("^\"?(.*)\"$", b)[0]
+                # print(a,b)
+                if a == "19":
+                    print(b)
+                x[a] = b
+            all_yuyan.append(x)
+    # print(all_yuyan)
+
+    with open("out.tex","w", encoding="utf-8") as texout:
+        for y in all_yuyan:
+            # index = y["index"]
+            title = y["title"] # .encode('utf-8').decode('unicode_escape')
+            content = y["content"] # .encode('utf-8').decode('unicode_escape')
+            conclusion = y["conclusion"]
+            print(y["title"])
+            texout.writelines("\\section{{{}}}\n\n".format(title))
+            texout.writelines("{}\n\n".format(content))
+            texout.writelines("{{\\bfseries \\color{{red}}{}}}\n\n".format(conclusion))
+    pass
+
+# re.findall("{",all)
+
+def main():
+    with open("../data/yisuodata.js") as jsin:
+        all = jsin.readlines()
+
+    proc_js(all)
+    # jiphy.to.python(all)
+
+if __name__ == "__main__":
+    main()
